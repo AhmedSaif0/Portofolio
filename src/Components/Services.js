dd61b825375022105b5f4a4dd94a59efa3656f66
AhmedSaif0 <71180230+AhmedSaif0@users.noreply.github.com>
2023-11-05 23:45:45 +0400

    slow hover fixed

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import '../Assets/Home.scss'
 import '../Assets/Services.scss'
-import {BsLinkedin,BsGithub,BsShare } from 'react-icons/bs'
-import { LiaLaptopCodeSolid} from 'react-icons/lia'
-import { BiSupport} from 'react-icons/bi'
+// import {BsLinkedin,BsGithub,BsShare } from 'react-icons/bs'
+// import { LiaLaptopCodeSolid} from 'react-icons/lia'
+// import { BiSupport} from 'react-icons/bi'
 import serv from '../Assets/images/serv (1).jpg'
 import social from '../Assets/images/social.jpg'
 import support from '../Assets/images/support.jpg'
@@ -15,9 +15,9 @@ const Services = () => {
         <h2 className='h2 serv' >My Services</h2>
         <div className='My-Serv flex'>
           <ul className='flex'>
-            <li> <div> <img src={serv} /> </div><div className='strong flex'><strong className='flex'> <span> Frontend Development </span> </strong> </div></li>
-            <li> <div> <img src={social} /> </div><div className='strong flex'><strong className='flex'> <span> Social Media Integration </span> </strong> </div></li>
-            <li> <div> <img src={support} /> </div><div className='strong flex'><strong className='flex'> <span> Maintenance and Support </span> </strong> </div></li>
+            <li> <div> <img src={serv} alt=''/> </div><div className='strong flex'><strong className='flex'> <span> Frontend Development </span> </strong> </div></li>
+            <li> <div> <img src={social} alt=''/> </div><div className='strong flex'><strong className='flex'> <span> Social Media Integration </span> </strong> </div></li>
+            <li> <div> <img src={support}alt='' /> </div><div className='strong flex'><strong className='flex'> <span> Maintenance and Support </span> </strong> </div></li>
           </ul>
         </div>
    
