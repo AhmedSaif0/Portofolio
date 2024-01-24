aa2e7ac40159c48bbba713893efcf1067639a785
Ahmed Saifaldeen <71180230+AhmedSaif0@users.noreply.github.com>
2024-01-24 23:19:37 +0400

    navbar2

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,25 +9,42 @@ const Header = () => {
 
 const [faBars, setFaBars] = useState(true);
   return(
-    <header className='section_nav'>
-      <Navbar className={ `${faBars? "header-big":"header-small"} `} >
-            <Navbar.Brand href="#home" className="navbar" > 
-               Ahmed
-            </Navbar.Brand>
-            <Nav className="nav-links">
-              <Nav.Link href="#about">About</Nav.Link>
-              <Nav.Link href="#services">Services</Nav.Link> 
-              <Nav.Link href="#contact">Contact</Nav.Link>
-            </Nav>
-              <div className='FaBars' >
-                <button 
-                    className='btnbar' 
-                    onClick={()=>setFaBars(!faBars)}>
-                    { faBars ?  (<><FaBars size={25}/></>):(<><FaTimes size={25}/></>)}
-                </button>
-              </div>
-      </Navbar>
-  </header>
+    // <header >
+      // {/* <Navbar className={ `${faBars? "header-big":"header-small"} `} >
+      //       <Navbar.Brand href="#home" className="navbar" > 
+      //          Ahmed
+      //       </Navbar.Brand>
+      //       <Nav className="nav-links">
+      //         <Nav.Link href="#about">About</Nav.Link>
+      //         <Nav.Link href="#services">Services</Nav.Link> 
+      //         <Nav.Link href="#contact">Contact</Nav.Link>
+      //       </Nav>
+      //         <div className='FaBars' >
+      //           <button 
+      //               className='btnbar' 
+      //               onClick={()=>setFaBars(!faBars)}>
+      //               { faBars ?  (<><FaBars size={25}/></>):(<><FaTimes size={25}/></>)}
+      //           </button>
+      //         </div>
+      // </Navbar> */}
+
+
+<Navbar collapseOnSelect expand="lg" className='section_nav sticky-top' >
+      <Container>
+        <Navbar.Brand href="#home">Ahmed</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto nav-links">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#services">Services</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+  // </header>
   )
 }
 
