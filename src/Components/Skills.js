b43802272bbb6e61fb617af7e318c45cdfb92240
Ahmed Saifaldeen <71180230+AhmedSaif0@users.noreply.github.com>
2024-02-11 13:16:52 +0400

    skills update

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -6,6 +6,8 @@ import {AiOutlineHtml5} from 'react-icons/ai'
 import {LiaCss3} from 'react-icons/lia'
 import {FaSass,FaReact} from 'react-icons/fa'
 import {TbBrandJavascript} from 'react-icons/tb'
+import { RiBootstrapFill } from "react-icons/ri";
+
 
 const Skills = () => {
   return (
@@ -15,6 +17,13 @@ const Skills = () => {
         <div className='My-skills flex container'>
           <ul className='flex'>
             <li> <span className='skill-icon-1'> <AiOutlineHtml5 /> </span><br/><strong> <span> Html</span> </strong> </li>
+            <li> <span className='skill-icon-1'> <LiaCss3/> </span><br/><strong> <span> Css </span> </strong> </li>
+            <li> <span className='skill-icon-1'> <FaSass/> </span><br/><strong> <span> Sass </span> </strong> </li>
+            <li> <span className='skill-icon-1'><TbBrandJavascript/> </span><br/><strong> <span> Javascript </span> </strong> </li>
+            <li> <span className='skill-icon-1'> <FaReact/> </span><br/><strong> <span> React js </span> </strong> </li>
+          </ul>
+          <ul className='flex'>
+            <li> <span className='skill-icon-1'> <RiBootstrapFill /></span><br/><strong> <span> Html</span> </strong> </li>
             <li> <span className='skill-icon-1'> <LiaCss3/> </span><br/><strong> <span> Css </span> </strong> </li>
             <li> <span className='skill-icon-1'> <FaSass/> </span><br/><strong> <span> Sass </span> </strong> </li>
             <li> <span className='skill-icon-1'><TbBrandJavascript/> </span><br/><strong> <span> Javascript </span> </strong> </li>
