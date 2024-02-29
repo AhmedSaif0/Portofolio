e2894e95c9e7172c4d791db64dda52f1fd2c25bc
Ahmed Saifaldeen <71180230+AhmedSaif0@users.noreply.github.com>
2024-02-29 18:02:41 +0400

    skills update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,18 +3,16 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import '../Assets/Header.scss'
-import {FaBars, FaTimes} from 'react-icons/fa'
 
 const Header = () => {
 
-const [faBars, setFaBars] = useState(true);
   return(
 
     <Navbar collapseOnSelect expand="lg" className='section_nav sticky-top' >
       <Container>
         <Navbar.Brand href="#home">Ahmed</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav" className='d-flex flex-row justify-content-lg-end'>
+        <Navbar.Collapse id="responsive-navbar-nav" className='justify-content-lg-end'>
           <Nav className="me-3 nav-links" >
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#services">Services</Nav.Link>
@@ -31,44 +29,3 @@ const [faBars, setFaBars] = useState(true);
 export default Header
 
 
-
-
-
-
-// import React, { useState } from 'react';
-// import { Navbar, Container, Nav } from 'react-bootstrap';
-// import { useMediaQuery } from 'react-responsive';
-
-// function NavigationBar() {
-//   const [showLinks, setShowLinks] = useState(false); // State to track link visibility
-
-//   // Define a media query for small screens
-//   const isSmallScreen = useMediaQuery({ maxWidth: 768 });
-
-//   // Function to toggle link visibility
-//   const toggleLinks = () => {
-//     setShowLinks(!showLinks);
-//   };
-
-//   return (
-//     <Navbar collapseOnSelect expand="lg" className='section_nav sticky-top'>
-//       <Container>
-//         <Navbar.Brand href="#home">Ahmed</Navbar.Brand>
-//         <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={toggleLinks} />
-//         <Navbar.Collapse id="responsive-navbar-nav" className='d-flex flex-row justify-content-lg-end'>
-//           {/* Conditionally render the Nav based on screen size and showLinks state */}
-//           {(isSmallScreen || showLinks) && (
-//             <Nav className="me-3 nav-links">
-//               <Nav.Link href="#about">About</Nav.Link>
-//               <Nav.Link href="#services">Services</Nav.Link>
-//               <Nav.Link href="#projects">Projects</Nav.Link>
-//               <Nav.Link href="#contact">Contact</Nav.Link>
-//             </Nav>
-//           )}
-//         </Navbar.Collapse>
-//       </Container>
-//     </Navbar>
-//   );
-// }
-
-// export default NavigationBar;
--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -4,9 +4,12 @@ import '../Assets/Home.scss'
 
 import {AiOutlineHtml5} from 'react-icons/ai'
 import {LiaCss3} from 'react-icons/lia'
-import {FaSass,FaReact} from 'react-icons/fa'
-import {TbBrandJavascript} from 'react-icons/tb'
+import {FaSass,FaReact,FaGithub} from 'react-icons/fa'
+import {TbBrandJavascript,TbBrandTypescript,TbBrandVite} from 'react-icons/tb'
 import { RiBootstrapFill } from "react-icons/ri";
+import { HiOutlineCommandLine } from "react-icons/hi2";
+
+
 
 
 const Skills = () => {
@@ -23,11 +26,12 @@ const Skills = () => {
             <li> <span className='skill-icon-1'> <FaReact/> </span><br/><strong> <span> React js </span> </strong> </li>
           </ul>
           <ul className='flex'>
-            <li> <span className='skill-icon-1'> <RiBootstrapFill /></span><br/><strong> <span> Html</span> </strong> </li>
-            <li> <span className='skill-icon-1'> <LiaCss3/> </span><br/><strong> <span> Css </span> </strong> </li>
-            <li> <span className='skill-icon-1'> <FaSass/> </span><br/><strong> <span> Sass </span> </strong> </li>
-            <li> <span className='skill-icon-1'><TbBrandJavascript/> </span><br/><strong> <span> Javascript </span> </strong> </li>
-            <li> <span className='skill-icon-1'> <FaReact/> </span><br/><strong> <span> React js </span> </strong> </li>
+            <li> <span className='skill-icon-1'> <RiBootstrapFill  style={{ color: '#8212f6' }} /></span><br/><strong> <span> bootstrap</span> </strong> </li>
+            <li> <span className='skill-icon-1'> <TbBrandTypescript style={{ color: '#2f74c0' }} /> </span><br/><strong> <span>Typescript</span> </strong> </li>
+            <li> <span className='skill-icon-1'> <FaGithub style={{ color: '#b965f4' }} /> </span><br/><strong> <span> GitHub </span> </strong> </li>
+            <li> <span className='skill-icon-1'><TbBrandVite style={{ color: '#ffaf07' }} /> </span><br/><strong> <span> VITE </span> </strong> </li>
+            <li> <span className='skill-icon-1'> <HiOutlineCommandLine style={{ color: '#fff' }} /> </span><br/><strong> <span> CLS </span> </strong> </li>
+
           </ul>
         </div>
       </div>
