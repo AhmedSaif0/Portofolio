5e69ad4fbe30f74dc4dd94d7da5342ddc6071de5
Ahmed Saifaldeen <71180230+AhmedSaif0@users.noreply.github.com>
2025-08-09 15:16:14 +0400

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -14,18 +14,12 @@ const about = () => {
         <div className='about-text'>
              <strong>I’m Ahmed Saifeldeen.</strong> <br />
               <p>
-                I've made the transition from a civil engineering background to a career in web development, 
-                specializing in front-end development. This professional pivot has been a great journey,
-                reflecting my determination to embrace change, learn new skills,
-                and navigate the dynamic landscape of technology.
-                With a foundation in civil engineering, I bring a unique perspective to problem-solving in web development. 
-                This narrative underscores the versatility and adaptability required to
-                reshape my career path and thrive as a front-end developer.
+I’m a front-end developer with over 5 years of experience in the IT industry, passionate about building smooth, user-friendly websites. I love learning new tech, solving problems, and turning ideas into real, functional products that people enjoy using.
             </p>
         </div>
         <div className='details'>
           <ul>
-              <li> <strong> <span> Age: </span> </strong> <span> 28 </span></li>
+              // <li> <strong> <span> Age: </span> </strong> <span> 28 </span></li>
               <li> <strong> <span> Residence: </span> </strong> <span> UAE </span></li>
               <li> <strong> <span> Freelance: </span> </strong> <span> Available </span></li>
               <li> <strong> <span> Address: </span> </strong> <span> Dubai, UAE </span></li>
